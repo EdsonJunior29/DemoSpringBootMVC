8a0e2b9f45d7981d0f4ba4cb21403c04a25a21cb
Edson Junior <edsonjos61@gmail.com>
2021-12-19 10:27:57 -0300

    Starting project Demo and Connection with DataBase MySQL.

---
+++ src/test/java/com/SpringBootMVC/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootMVC.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
