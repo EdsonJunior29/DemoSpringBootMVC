a926a2d4e983c0c6da9cd335d47d9ef3d0e1b65c
Edson Junior <edsonjos61@gmail.com>
2021-12-22 08:41:27 -0300

    working with HTML em Thymeleaf.

--- src/main/java/com/SpringBootMVC/demo/domain/User.java
+++ src/main/java/com/SpringBootMVC/demo/domain/User.java
@@ -27,6 +27,8 @@ public class User {
 	@Column(length = 45 , nullable = false)
 	private String lastName;
 
+	private boolean enabled;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -65,6 +67,14 @@ public class User {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
 	}
 	
 	
